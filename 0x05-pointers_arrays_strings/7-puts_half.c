7191813628a38107faad1685d872d2f2391744b1
NathanBaraka <nathanbaraka23@gmail.com>
2023-08-21 07:43:05 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,11 +7,11 @@
  */
 void puts_half(char *str)
 {
-	int t, n, longi;
+	int a, n, longi;
 
 	longi = 0;
 
-	for (t = 0; str[t] != '\0'; t++)
+	for (a = 0; str[a] != '\0'; a++)
 		longi++;
 
 	n = (longi / 2);
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
 
-	for (t = n; str[t] != '\0'; t++)
+	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
